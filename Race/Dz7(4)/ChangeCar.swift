36b14008189ba1779f8a36bcfbc4b5bc7446cede
Elijah <jugul01223@gmail.com>
2021-05-09 20:35:39 +0300

    no message

---
+++ Race/Dz7(4)/ChangeCar.swift
@@ -0,0 +1,14 @@
+//
+//  ChangeCar.swift
+//  Dz7(4)
+//
+//  Created by Elijah Malashko on 1.05.21.
+//
+
+import Foundation
+
+struct ChangeCar {
+    static var carObject = false
+    static var carObject2 = false
+    static var carObject3 = false
+}
---
+++ Race/Dz7(4)/Score.swift
@@ -0,0 +1,13 @@
+//
+//  Score.swift
+//  Dz7(4)
+//
+//  Created by Elijah Malashko on 2.05.21.
+//
+
+import Foundation
+import UIKit
+
+struct ScoreNumber: Codable {
+    static var score: Int = 0
+}
